05568001a17ec5635d30bb9e848528a0410c46b7
XCL <80213876@qq.com>
2016-01-23 15:54:29 +0800

    update

--- XCLWinKits/DateTimeDIFF/Index.Designer.cs
+++ XCLWinKits/DateTimeDIFF/Index.Designer.cs
@@ -29,46 +29,77 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.txtEndTime = new System.Windows.Forms.DateTimePicker();
+            this.txtStartTime = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbMsg = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.txtEndTime);
+            this.groupBox1.Controls.Add(this.txtStartTime);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(700, 100);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "设置时间：";
             // 
-            // dateTimePicker1
+            // txtEndTime
             // 
-            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
-            this.dateTimePicker1.Location = new System.Drawing.Point(32, 41);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(167, 21);
-            this.dateTimePicker1.TabIndex = 0;
+            this.txtEndTime.Location = new System.Drawing.Point(412, 41);
+            this.txtEndTime.Name = "txtEndTime";
+            this.txtEndTime.Size = new System.Drawing.Size(200, 21);
+            this.txtEndTime.TabIndex = 1;
+            this.txtEndTime.ValueChanged += new System.EventHandler(this.txtEndTime_ValueChanged);
             // 
-            // dateTimePicker2
+            // txtStartTime
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(271, 41);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
-            this.dateTimePicker2.TabIndex = 1;
+            this.txtStartTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.txtStartTime.Location = new System.Drawing.Point(114, 41);
+            this.txtStartTime.Name = "txtStartTime";
+            this.txtStartTime.Size = new System.Drawing.Size(200, 21);
+            this.txtStartTime.TabIndex = 0;
+            this.txtStartTime.ValueChanged += new System.EventHandler(this.txtStartTime_ValueChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbMsg);
+            this.groupBox2.Location = new System.Drawing.Point(12, 138);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(700, 335);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "间隔：";
+            // 
+            // lbMsg
+            // 
+            this.lbMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbMsg.AutoSize = true;
+            this.lbMsg.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbMsg.ForeColor = System.Drawing.Color.Green;
+            this.lbMsg.Location = new System.Drawing.Point(328, 135);
+            this.lbMsg.Name = "lbMsg";
+            this.lbMsg.Size = new System.Drawing.Size(0, 46);
+            this.lbMsg.TabIndex = 0;
             // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 485);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Index";
             this.Text = "Index";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -76,7 +107,9 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker txtEndTime;
+        private System.Windows.Forms.DateTimePicker txtStartTime;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbMsg;
     }
 }
--- XCLWinKits/DateTimeDIFF/Index.cs
+++ XCLWinKits/DateTimeDIFF/Index.cs
@@ -16,6 +16,33 @@ namespace DateTimeDIFF
         public Index()
         {
             InitializeComponent();
+
+            this.txtStartTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.txtStartTime.Format = DateTimePickerFormat.Custom;
+            this.txtStartTime.ShowUpDown = true;
+
+            this.txtEndTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.txtEndTime.Format = DateTimePickerFormat.Custom;
+            this.txtEndTime.ShowUpDown = true;
+
+            this.Process();
+        }
+
+        private void Process()
+        {
+            DateTime start = XCLNetTools.StringHander.Common.GetDateTime(this.txtStartTime.Text);
+            DateTime end = XCLNetTools.StringHander.Common.GetDateTime(this.txtEndTime.Text);
+            this.lbMsg.Text= XCLNetTools.StringHander.DateHelper.GetTimeString(start,end);
+        }
+
+        private void txtStartTime_ValueChanged(object sender, EventArgs e)
+        {
+            this.Process();
+        }
+
+        private void txtEndTime_ValueChanged(object sender, EventArgs e)
+        {
+            this.Process();
         }
 
         public override string SubAssemblyName
