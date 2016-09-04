b376951abb296f612eb467476936caf5d200d85e
XCL <80213876@qq.com>
2016-09-04 11:59:54 +0800

    更新【日期时间差】计算功能

--- XCLWinKits/DateTimeDIFF/Index.Designer.cs
+++ XCLWinKits/DateTimeDIFF/Index.Designer.cs
@@ -29,75 +29,108 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtEndTime = new System.Windows.Forms.DateTimePicker();
-            this.txtStartTime = new System.Windows.Forms.DateTimePicker();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEndTime = new System.Windows.Forms.TextBox();
+            this.txtStartTime = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lbMsg = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnCount);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtEndTime);
             this.groupBox1.Controls.Add(this.txtStartTime);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(700, 100);
+            this.groupBox1.Size = new System.Drawing.Size(398, 102);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "设置时间：";
             // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(303, 40);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 4;
+            this.btnCount.Text = "计算";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "结束时间：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "开始时间：";
+            // 
             // txtEndTime
             // 
-            this.txtEndTime.Location = new System.Drawing.Point(412, 41);
+            this.txtEndTime.Location = new System.Drawing.Point(98, 57);
             this.txtEndTime.Name = "txtEndTime";
-            this.txtEndTime.Size = new System.Drawing.Size(200, 21);
+            this.txtEndTime.Size = new System.Drawing.Size(183, 21);
             this.txtEndTime.TabIndex = 1;
-            this.txtEndTime.ValueChanged += new System.EventHandler(this.txtEndTime_ValueChanged);
             // 
             // txtStartTime
             // 
-            this.txtStartTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
-            this.txtStartTime.Location = new System.Drawing.Point(114, 41);
+            this.txtStartTime.Location = new System.Drawing.Point(98, 20);
             this.txtStartTime.Name = "txtStartTime";
-            this.txtStartTime.Size = new System.Drawing.Size(200, 21);
+            this.txtStartTime.Size = new System.Drawing.Size(183, 21);
             this.txtStartTime.TabIndex = 0;
-            this.txtStartTime.ValueChanged += new System.EventHandler(this.txtStartTime_ValueChanged);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.lbMsg);
-            this.groupBox2.Location = new System.Drawing.Point(12, 138);
+            this.groupBox2.Controls.Add(this.txtResult);
+            this.groupBox2.Location = new System.Drawing.Point(12, 120);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(700, 335);
+            this.groupBox2.Size = new System.Drawing.Size(398, 278);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "间隔：";
             // 
-            // lbMsg
+            // txtResult
             // 
-            this.lbMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbMsg.AutoSize = true;
-            this.lbMsg.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lbMsg.ForeColor = System.Drawing.Color.Green;
-            this.lbMsg.Location = new System.Drawing.Point(328, 135);
-            this.lbMsg.Name = "lbMsg";
-            this.lbMsg.Size = new System.Drawing.Size(0, 46);
-            this.lbMsg.TabIndex = 0;
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Location = new System.Drawing.Point(3, 17);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(392, 258);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.WordWrap = false;
             // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 485);
+            this.ClientSize = new System.Drawing.Size(422, 410);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Index";
             this.Text = "Index";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -107,9 +140,12 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DateTimePicker txtEndTime;
-        private System.Windows.Forms.DateTimePicker txtStartTime;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label lbMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEndTime;
+        private System.Windows.Forms.TextBox txtStartTime;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
--- XCLWinKits/DateTimeDIFF/Index.cs
+++ XCLWinKits/DateTimeDIFF/Index.cs
@@ -1,46 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DateTimeDIFF
 {
     public partial class Index : BaseForm.BaseFormClass
     {
+        private const long oneYearMilliseconds = 365 * 24 * 3600 * 1000L;//一年的毫秒数
+        private const long oneMonthMilliseconds = 30 * 24 * 3600 * 1000L;//一个月的毫秒数
+        private const long oneDayMilliseconds = 24 * 3600 * 1000L;//一天的毫秒数
+        private const long oneHourMilliseconds = 3600 * 1000L;//一小时的毫秒数
+
         public Index()
         {
             InitializeComponent();
 
-            this.txtStartTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
-            this.txtStartTime.Format = DateTimePickerFormat.Custom;
-            this.txtStartTime.ShowUpDown = true;
-
-            this.txtEndTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
-            this.txtEndTime.Format = DateTimePickerFormat.Custom;
-            this.txtEndTime.ShowUpDown = true;
-
-            this.Process();
+            this.txtStartTime.Text = string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}", DateTime.Now);
+            this.txtEndTime.Text = string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}", DateTime.Now.AddDays(7));
         }
 
         private void Process()
         {
-            DateTime start = XCLNetTools.StringHander.Common.GetDateTime(this.txtStartTime.Text);
-            DateTime end = XCLNetTools.StringHander.Common.GetDateTime(this.txtEndTime.Text);
-            this.lbMsg.Text= XCLNetTools.StringHander.DateHelper.GetTimeString(start,end);
+            var start = XCLNetTools.StringHander.Common.GetDateTimeNullable(this.txtStartTime.Text);
+            var end = XCLNetTools.StringHander.Common.GetDateTimeNullable(this.txtEndTime.Text);
+            if (!start.HasValue)
+            {
+                MessageBox.Show("请输入有效格式的开始时间！", "系统提示");
+                return;
+            }
+            if (!end.HasValue)
+            {
+                MessageBox.Show("请输入有效格式的结束时间！", "系统提示");
+                return;
+            }
+
+            var resultLst = new List<string>();
+            var timeSpan = end.Value.Subtract(start.Value);
+            var totalMilliseconds = timeSpan.TotalMilliseconds;
+            var longTotalMilliseconds = (long)totalMilliseconds;
+
+            //总毫秒数
+            resultLst.Add(string.Format("总毫秒数：{0}", totalMilliseconds));
+
+            //总秒数
+            resultLst.Add(string.Format("总秒数：{0}", timeSpan.TotalSeconds));
+
+            //总分数
+            resultLst.Add(string.Format("总分数：{0}", timeSpan.TotalMinutes));
+
+            //总小时数
+            resultLst.Add(string.Format("总小时数：{0}", timeSpan.TotalHours));
+
+            //总天数
+            resultLst.Add(string.Format("总天数：{0}", timeSpan.TotalDays));
+
+            //总月数
+            resultLst.Add(string.Format("总月数：{0}", timeSpan.TotalDays / 30.0));
+
+            //总年数
+            resultLst.Add(string.Format("总年数：{0}", timeSpan.TotalDays / 365.0));
+
+            //xx年xx月xx天xx小时xx分xx秒xx毫秒
+            resultLst.Add(string.Format("间隔：{0}年{1}个月{2}天{3}小时{4}分钟{5}秒{6}毫秒",
+                longTotalMilliseconds / oneYearMilliseconds,
+                (longTotalMilliseconds % oneYearMilliseconds) / oneMonthMilliseconds,
+                (longTotalMilliseconds % oneYearMilliseconds % oneMonthMilliseconds) / oneDayMilliseconds,
+                (longTotalMilliseconds % oneYearMilliseconds % oneMonthMilliseconds % oneDayMilliseconds) / oneHourMilliseconds,
+                 (longTotalMilliseconds % oneYearMilliseconds % oneMonthMilliseconds % oneDayMilliseconds % oneHourMilliseconds) / 60000,
+                 (longTotalMilliseconds % oneYearMilliseconds % oneMonthMilliseconds % oneDayMilliseconds % oneHourMilliseconds % 60000) / 1000,
+                 longTotalMilliseconds % oneYearMilliseconds % oneMonthMilliseconds % oneDayMilliseconds % oneHourMilliseconds % 60000 % 1000
+                ));
+
+            this.txtResult.Text = string.Join(Environment.NewLine + Environment.NewLine, resultLst.ToArray());
         }
 
-        private void txtStartTime_ValueChanged(object sender, EventArgs e)
-        {
-            this.Process();
-        }
-
-        private void txtEndTime_ValueChanged(object sender, EventArgs e)
+        private void btnCount_Click(object sender, EventArgs e)
         {
             this.Process();
         }
