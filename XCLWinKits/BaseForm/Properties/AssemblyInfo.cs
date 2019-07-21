4b3ed6180b798b86f958266eddd6c42e75b16369
xucongli1989 <80213876@qq.com>
2019-07-21 18:44:00 +0800

    更新版本号

--- XCLWinKits/BaseForm/Properties/AssemblyInfo.cs
+++ XCLWinKits/BaseForm/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.4.6")]
-[assembly: AssemblyFileVersion("1.4.6")]
+[assembly: AssemblyVersion("1.4.7")]
+[assembly: AssemblyFileVersion("1.4.7")]
